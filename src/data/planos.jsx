556c854bc9d62d245dea9220beae4dd1d33c367f
Bruunohfs <bruuno.henrique.1996@gmail.com>
2025-09-05 17:30:23 -0300

    correção de preços dos planos

--- src/data/planos.jsx
+++ src/data/planos.jsx
@@ -5,28 +5,27 @@ export const planosMensais = [
       id: 1, 
       nome: "Individual", 
       descricao: "Perfeito para você",
-      preco: "34,90",
+      preco: "29,90",
       economia: "",
       beneficios: [
         "Todas as especialidades",
         "Consultas ilimitadas",
         "Sem cobranças extras",
         "Dependentes Inclusos", 
-        "Veterinario para cães e gatos"
+        "Veterinario para cães e gatos",
+        "Clube de vantagens"
       ],
       checkoutUrl: defaultCheckoutUrl 
     },
     { 
       id: 2, 
-      nome: "Individual Farma", 
+      nome: "Individual Plus", 
       descricao: "Individual + benefícios farmácia",
-      preco: "54,90",
+      preco: "49,90",
       economia: "",
       beneficios: [
         "Tudo do Individual",
-        "R$ 30 mensais Pix Farma",
-        "Descontos em medicamentos",
-        "Clube de vantagens"
+        "+ R$ 30 mensais Pix Farma",
       ],
       checkoutUrl: defaultCheckoutUrl 
     },
@@ -34,7 +33,7 @@ export const planosMensais = [
       id: 3, 
       nome: "Família", 
       descricao: "Para toda a família",
-      preco: "69,90",
+      preco: "59,90",
       economia: "",
       beneficios: [
         "Duas licenças Adulto",
@@ -42,21 +41,20 @@ export const planosMensais = [
         "Consultas ilimitadas",
         "Sem cobranças extras",
         "Dependentes Inclusos",
-        "Veterinario para cães e gatos"
+        "Veterinario para cães e gatos",
+        "Clube de vantagens"
       ],
       checkoutUrl: defaultCheckoutUrl 
     },
     { 
       id: 4, 
-      nome: "Família Farma", 
+      nome: "Família Plus", 
       descricao: "Família + benefícios farmácia",
-      preco: "99,90",
+      preco: "89,90",
       economia: "",
       beneficios: [
         "Tudo do Família",
-        "R$ 60 mensais Pix Farma",
-        "Descontos em medicamentos",
-        "Clube de vantagens"
+        "+ R$ 60 mensais Pix Farma"
       ],
       checkoutUrl: defaultCheckoutUrl 
     }
@@ -67,30 +65,29 @@ export const planosAnuais = [
       id: 1, 
       nome: "Individual", 
       descricao: "Perfeito para você",
-      preco: "25,90",
+      preco: "24,90",
       parcelamento: "12x de",
-      economia: "Economize R$ 72/ano",
+      economia: "Economize R$ 60/ano",
       beneficios: [
         "Todas as especialidades",
         "Consultas ilimitadas",
         "Sem cobranças extras",
         "Dependentes Inclusos", 
-        "Veterinario para cães e gatos"
+        "Veterinario para cães e gatos",
+        "Clube de vantagens"
       ],
       checkoutUrl: defaultCheckoutUrl 
     },
     { 
       id: 2, 
-      nome: "Individual Farma", 
+      nome: "Individual Plus", 
       descricao: "Individual + benefícios farmácia",
-      preco: "44,90",
+      preco: "39,90",
       parcelamento: "12x de",
       economia: "Economize R$ 120/ano",
       beneficios: [
         "Tudo do Individual",
-        "R$ 30 mensais Pix Farma",
-        "Descontos em medicamentos",
-        "Clube de vantagens"
+        "R$ 30 mensais Pix Farma"
       ],
       checkoutUrl: defaultCheckoutUrl,
       destaque: true
@@ -99,7 +96,7 @@ export const planosAnuais = [
       id: 3, 
       nome: "Família", 
       descricao: "Para toda a família",
-      preco: "54,90",
+      preco: "49,90",
       parcelamento: "12x de",
       economia: "Economize R$ 120/ano",
       beneficios: [
@@ -108,22 +105,21 @@ export const planosAnuais = [
         "Consultas ilimitadas",
         "Sem cobranças extras",
         "Dependentes Inclusos",
-        "Veterinario para cães e gatos"
+        "Veterinario para cães e gatos",
+        "Clube de vantagens"
       ],
       checkoutUrl: defaultCheckoutUrl 
     },
     { 
       id: 4, 
-      nome: "Família Farma", 
+      nome: "Família Plus", 
       descricao: "Família + benefícios farmácia",
       preco: "79,90",
       parcelamento: "12x de",
-      economia: "Economize R$ 240/ano",
+      economia: "Economize R$ 120/ano",
       beneficios: [
         "Tudo do Família",
         "R$ 60 mensais Pix Farma",
-        "Descontos em medicamentos",
-        "Clube de vantagens"
       ],
       checkoutUrl: defaultCheckoutUrl 
     },
