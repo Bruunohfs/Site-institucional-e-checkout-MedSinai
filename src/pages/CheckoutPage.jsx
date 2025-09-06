58d9e3c515bab8eaa316e5456c906e2496399311
Bruunohfs <bruuno.henrique.1996@gmail.com>
2025-09-05 23:29:48 -0300

    envia referencia de parceiro no checkout

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -12,6 +12,17 @@ const LockIcon = () => (
   </svg>
 );
 
+function getCookie(name) {
+    const nameEQ = name + "=";
+    const ca = document.cookie.split(';');
+    for(let i=0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 function CheckoutPage() {
   const { tipoPlano, idDoPlano } = useParams();
   const arrayDeBusca = tipoPlano === 'anual' ? planosAnuais : planosMensais;
@@ -97,6 +108,8 @@ if (tipoPlano === 'anual' && planoSelecionado) {
     setIsProcessing(true);
     setPaymentResult(null);
 
+    const partnerId = getCookie('medsinai_partner_ref');
+
     const dadosCompletos = {
       plano: {
         nome: planoSelecionado.nome,
@@ -104,6 +117,8 @@ if (tipoPlano === 'anual' && planoSelecionado) {
         tipo: tipoPlano,
       },
       cliente: data,
+
+      ReferenciaParceiro: partnerId || 'venda_direta'
     };
 
     try {
